37769a3082ee4cd9e700423470edd22a4f5f5bf7
dat <quocdatit2k10@gmail.com>
2021-08-08 13:27:53 +1000

    First commit

---
+++ src/test/java/au/xero/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package au.xero.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
