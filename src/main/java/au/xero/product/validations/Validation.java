f838da61f7189055694ec5670754a1c8b1d573e0
dat <quocdatit2k10@gmail.com>
2021-08-11 09:04:11 +1000

    clean code

--- src/main/java/au/xero/product/validations/Validation.java
+++ src/main/java/au/xero/product/validations/Validation.java
@@ -44,19 +44,4 @@ public class Validation {
         return true;
     }
 
-
-    public void validate(Object object, Errors errors) {
-
-        User user = (User) object;
-
-        if(user.getPassword().length() <6){
-            errors.rejectValue("password","Length", "Password must be at least 6 characters");
-        }
-
-        if(!user.getPassword().equals(user.getConfirmPassword())){
-            errors.rejectValue("confirmPassword","Match", "Passwords must match");
-
-        }
-    }
-
 }
